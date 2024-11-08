5b22b2fb589622a5556a7cb3d7526201dfb4d48b
Quentin Rocher <qrocher@weyield.io>
2024-11-08 15:42:15 +0100

    Possibilité d'inserer dans la table annonce, personne et logement

--- Model/exemple.php
+++ Model/exemple.php
@@ -17,15 +17,30 @@ foreach ($resultats as $personne) {
 echo "<br>";
 
 // Exemple d'utilisation de la méthode supprimerById
-// Supprimer l'personne avec l'ID 1
-$idASupprimer = 2;
+// Supprimer l'personne avec l'ID 10
+$idASupprimer = 10;
 $crud->supprimerById($table, $idASupprimer);
 echo "L'utilisateur avec l'ID $idASupprimer a été supprimé.";
-$resultats = $crud->selectAllFromTable($table);
+echo "<br>";
+// Exemple d'utilisation de la méthode insertInTable
+// Ajouter une nouvelle personne dans la table 'Personne'
+$nom = "Doe";
+$prenom = "John";
+$email = "john.doe@emaple.com";
+$actif = true;
+$telephone = "1234568&a90";
+$mdp = password_hash("password123", PASSWORD_BCRYPT);
 
+$isInsert = $crud->insererPersonne($nom, $prenom, $email, $actif, $telephone, $mdp);
+if ($isInsert) {
+    echo "L'utilisateur $prenom $nom a été ajouté avec succès.";
+} else {
+    echo "Erreur lors de l'ajout de l'utilisateur.";
+}
 echo "<br>";
 echo "<br>";
 // Afficher les résultats
+$resultats = $crud->selectAllFromTable($table);
 echo "Liste des Personnes :<br>";
 foreach ($resultats as $personne) {
     echo "ID: " . $personne['id'] . " - Nom: " . $personne['nom'] . " - Prénom: " . $personne['prénom'] . "- Email: " . $personne["email"] . "<br>";
