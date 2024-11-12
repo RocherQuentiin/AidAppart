b90b6c34f5ed3e21388c6b4c5b27421e5e4e63c8
fathi marie <fathi.marie1@gmail.com>
2024-11-12 16:51:16 +0100

    Ajout de la page header

---
+++ Vue/Layout/footer.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Barre de navigation responsive sans Bootstrap</title>
+        <link rel="stylesheet" href="../css/stylesheet.css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    </head>
+    <body>
+        <footer class="footer row">
+                <ul class="logement col-lg-3">
+                    <li class="titre">Louer mon logement</li>
+                    <li>Logement étudiant</li>
+                    <li>Résidence étudiante</li>
+                    <li>Colocation</li>
+                    <li>Appartement</li>
+                    <li>Studio</li>
+                    <li>Chambre chez l’habitant</li>
+                </ul>
+                <ul class="etudiant col-lg-3">
+                    <li class="titre">Logement étudiant en France</li>
+                    <li>Logement étudiant à Paris</li>
+                    <li>Logement étudiant à Lille</li>
+                    <li>Logement étudiant à Lyon</li>
+                    <li>Logement étudiant à Toulouse</li>
+                    <li>Logement étudiant à Marseille</li>
+                </ul>
+                <ul class="aPropos col-lg-2">
+                    <li class="titre">À propos</li>
+                    <li>Blog</li>
+                    <li>Actualités</li>
+                    <li>Écoles et universités partenaires</li>
+                    <li>Mentions légales</li>
+                    <li>CGU</li>
+                    <li>FAQ</li>
+                </ul>
+            <ul class="partDroite col-lg-3 row">
+                <div class="logo">
+                    <li class="titre"><img alt="AidAppart" src="logo.png"></li>
+                    <li>Connexion</li>
+                    <li>Espace propriétaire</li>
+                    <li>Espace professionnel de </br>l’immobilier</li>
+                    <li>Gérer les cookies</li>
+                    <ul class="row reseaux">
+                        <li><img alt="insta" src="globe.png"/></li>
+                        <li><img alt="facebook" src="globe.png"/></li>
+                        <li><img alt="X" src="globe.png"/></li>
+                        <li><img alt="Snapchat" src="globe.png"/></li>
+                    </ul>
+                </div>
+                <div class="espace_droite">
+
+                </div>
+            </ul>
+        </footer>
+    </body>
+</html>
