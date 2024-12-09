e6a32a4dc8a853ff2164ab81b4e48abf603e4d6c
Danré <hippolyte.danre@gmail.com>
2024-12-09 09:57:52 +0100

    retirer les frames works

--- Views/view_connexion.php
+++ Views/view_connexion.php
@@ -5,27 +5,20 @@
     <title>Connexion</title>
     <link href="Content/css/Connexion.css" rel="stylesheet">
     <link rel="stylesheet" href="Content/css/index.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
  
     <div class="centre">
         <h1>JE ME CONNECTE</h1>
-
-        <!-- Groupe de champs email -->
         <div class="input-group">
             <img src="../Content/images/email.png" alt="Email Icon" class="icon">
             <input type="email" placeholder="Votre adresse email" required>
         </div>
-
-        <!-- Groupe de champs mot de passe -->
         <div class="input-group">
             <img src="../Content/images/cadenas.png" alt="Lock Icon" class="icon">
             <input type="password" placeholder="Votre mot de passe" id="password" required>
             <img src="../Content/images/eye_icon.png" alt="Eye Icon" class="eye-icon" id="eye-toggle" onclick="togglePassword()">
         </div>
-
-        <!-- Liens -->
         <a href="view_inscription.php" class="link">Première connexion ? Je crée un compte AidAppart</a>
         <a href="view_mot_de_passe_oublie.php" class="link">Mot de passe oublié ?</a>
 
@@ -33,7 +26,6 @@
         <button class="button">Me Connecter</button>
     </div>
 
-    <!-- Script pour basculer la visibilité du mot de passe -->
     <script>
         function togglePassword() {
             const passwordInput = document.getElementById('password');
