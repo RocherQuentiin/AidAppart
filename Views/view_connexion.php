6c9c9a78fb837cc9755fefb3ce5c23b388e9fa7a
Danré <hippolyte.danre@gmail.com>
2024-12-09 09:57:52 +0100

    retirer les frames works

--- Views/view_connexion.php
+++ Views/view_connexion.php
@@ -5,7 +5,6 @@
     <title>Connexion</title>
     <link href="../content/css/Connexion.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/index.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
  
