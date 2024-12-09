72a53b2f926930ed58943704f6fa941a2b6c9751
Danré <hippolyte.danre@gmail.com>
2024-12-09 09:36:51 +0100

    changement connexion en view_connexion.php

--- Views/view_mot_de_passe_oublie.php
+++ Views/view_mot_de_passe_oublie.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mot de Passe Oublié</title>
-    <link href="css/Connexion.css" rel="stylesheet">
+    <link href="../content/css/Connexion.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
@@ -16,7 +16,7 @@
             <input type="email" placeholder="Votre adresse mail" required>
         </div>
         <button class="btn">Réinitialiser mon mot de passe</button>
-        <a href="connexion.php" class="link">Retour à la connexion</a>
+        <a href="view_connexion.php" class="link">Retour à la connexion</a>
     </div>
 
 </body>
