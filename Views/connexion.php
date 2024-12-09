6a0534c83d9dcb641c92f1b551c5f9c0b29175f0
Danré <hippolyte.danre@gmail.com>
2024-12-09 09:19:13 +0100

    Commit avant pull request

--- Views/connexion.php
+++ Views/connexion.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link href="css/Connexion.css" rel="stylesheet">
+    <link href="../content/css/Connexion.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/index.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
