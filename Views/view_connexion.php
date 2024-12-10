8a9e31cd03c837736dd8419f0d47287067b7770b
Danré <hippolyte.danre@gmail.com>
2024-12-10 16:21:27 +0100

    Commencement php

--- Views/view_connexion.php
+++ Views/view_connexion.php
@@ -1,24 +1,36 @@
+<?php
+require_once __DIR__ . '/../controller_connexion.php'; // Inclut le contrôleur
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link href="Content/css/Connexion.css" rel="stylesheet">
-    <link rel="stylesheet" href="Content/css/index.css"/>
+
+    <link href="../Content/css/connexion.css" rel="stylesheet">
+    <link rel="stylesheet" href="../Content/css/index.css"/>
 </head>
 <body>
- 
+
     <div class="centre">
         <h1>JE ME CONNECTE</h1>
+
+        <!-- Groupe de champs email -->
         <div class="input-group">
             <img src="../Content/images/email.png" alt="Email Icon" class="icon">
             <input type="email" placeholder="Votre adresse email" required>
         </div>
+
+        <!-- Groupe de champs mot de passe -->
         <div class="input-group">
             <img src="../Content/images/cadenas.png" alt="Lock Icon" class="icon">
             <input type="password" placeholder="Votre mot de passe" id="password" required>
             <img src="../Content/images/eye_icon.png" alt="Eye Icon" class="eye-icon" id="eye-toggle" onclick="togglePassword()">
         </div>
+
+        <!-- Liens -->
         <a href="view_inscription.php" class="link">Première connexion ? Je crée un compte AidAppart</a>
         <a href="view_mot_de_passe_oublie.php" class="link">Mot de passe oublié ?</a>
 
@@ -26,6 +38,7 @@
         <button class="button">Me Connecter</button>
     </div>
 
+    <!-- Script pour basculer la visibilité du mot de passe -->
     <script>
         function togglePassword() {
             const passwordInput = document.getElementById('password');
