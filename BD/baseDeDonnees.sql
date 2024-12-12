54481987cff73f612b80577c9b95681d90c38be3
Quentin <prorocher.quentin@gmail.com>
2024-12-12 23:33:37 +0100

    suppression de doublon de table dans la base de données

--- BD/baseDeDonnees.sql
+++ BD/baseDeDonnees.sql
@@ -59,24 +59,10 @@ CREATE TABLE Maison (
     FOREIGN KEY (id_logement) REFERENCES Logement(id)
 );
 
-CREATE TABLE Maison (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_logement INTEGER NOT NULL,
-    nb_etages INTEGER NOT NULL,
-    a_jardin BOOLEAN NOT NULL,
-    FOREIGN KEY (id_logement) REFERENCES Logement(id)
-);
 
-CREATE TABLE Maison (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_logement INTEGER NOT NULL,
-    nb_etages INTEGER NOT NULL,
-    a_jardin BOOLEAN NOT NULL,
-    FOREIGN KEY (id_logement) REFERENCES Logement(id)
-);
 
 CREATE TABLE Appartement (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     id_logement INTEGER NOT NULL,
     etage INTEGER NOT NULL,
     a_ascenseur BOOLEAN NOT NULL,
@@ -85,25 +71,6 @@ CREATE TABLE Appartement (
     FOREIGN KEY (id_logement) REFERENCES Logement(id)
 );
 
-CREATE TABLE Appartement (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_logement INTEGER NOT NULL,
-    etage INTEGER NOT NULL,
-    a_ascenseur BOOLEAN NOT NULL,
-    a_balcon BOOLEAN NOT NULL,
-    a_concierge BOOLEAN NOT NULL,
-    FOREIGN KEY (id_logement) REFERENCES Logement(id)
-);
-
-CREATE TABLE Appartement (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_logement INT NOT NULL,
-    etage INT NOT NULL,
-    a_ascenseur BOOLEAN NOT NULL,
-    a_balcon BOOLEAN NOT NULL,
-    a_concierge BOOLEAN NOT NULL,
-    FOREIGN KEY (id_logement) REFERENCES Logement(id)
-);
 
 CREATE TABLE Avis (
     id INT PRIMARY KEY AUTO_INCREMENT,
