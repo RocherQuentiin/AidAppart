385b87941ae00235f06a36d452aafd17efd60db2
Quentin <prorocher.quentin@gmail.com>
2024-12-18 01:19:56 +0100

    delete useless code

--- Utils/config.php
+++ Utils/config.php
@@ -13,14 +13,6 @@ if ( $environment == "local") {
     $dbname = getenv("MYSQL_DATABASE");
 }
 
-
-
-$servername = 'localhost:3306';
-$username = 'default_user';
-$password = 'AidappartNova';
-$dbname = 'Aidappart';
-
-
 global $TABLES;
 $TABLES = [
     'PERSONNE' => 'Personne',
