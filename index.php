d784deb45844554c37c95b3488a4935fa2d5faa0
Touline <toulinechaouachi@yahoo.fr>
2025-01-08 19:27:11 +0100

    ok

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ require_once "Models/Model.php";
 require_once "Controllers/Controller.php";
 
 //Liste des contrôleurs -- A RENSEIGNER
-$controllers = ["admin", "inscription","connexion","deconnexion","pagelogement","accueil","aide","mdp_oublie","changement_mdp", "annonces", "ajoutLogement"];
+$controllers = ["admin", "inscription","connexion","deconnexion","pagelogement","accueil","aide","mdp_oublie","changement_mdp", "annonces", "ajoutLogement","recherche"];
 //Nom du contrôleur par défaut-- A RENSEIGNER
-$controller_default = "accueil";
+$controller_default = "recherche";
 
 //On teste si le paramètre controller existe et correspond à un contrôleur de la liste $controllers
 if (isset($_GET['controller']) and in_array($_GET['controller'], $controllers)) {
